1b39189231b4e1f6cce35594feb545ec1e78c540
Manav Aggarwal <53603898+KnightCoder02@users.noreply.github.com>
2023-07-08 20:10:05 +0530

    Add files via upload

--- cpp/June 29/Take Input from User.cpp
+++ cpp/June 29/Take Input from User.cpp
@@ -2,7 +2,16 @@
 using namespace std;
 int main(){
     int a;
+    int b;
+
     cin >> a;
+    cin >> b;
+    
     cout << "Value of a: ";
     cout << a;
+
+    cout << "\n";
+
+    cout << "Value of b: ";
+    cout << b;
 }
