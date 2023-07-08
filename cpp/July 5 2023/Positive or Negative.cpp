b0b3d1c2959aedff83b4fe2fe67f7cdf1f435c44
Manav Aggarwal <53603898+KnightCoder02@users.noreply.github.com>
2023-07-08 23:55:43 +0530

    Add files via upload

--- cpp/July 5 2023/Positive or Negative.cpp
+++ cpp/July 5 2023/Positive or Negative.cpp
@@ -1,1 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin >> n;
 
+    if(n == 0){
+        cout << "You entered 0";
+    }
+    else if (n > 0){
+        cout << "Positive number";
+    }
+    else{
+        cout << "Negative number";
+    }
+}
