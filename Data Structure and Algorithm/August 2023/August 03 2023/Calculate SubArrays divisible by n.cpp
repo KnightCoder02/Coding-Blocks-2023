13fe8283ff5f9fdc12da5e2c388d0028f00e0b32
Manav Aggarwal <53603898+Manav-Agg@users.noreply.github.com>
2023-08-05 23:36:05 +0530

    Add files via upload

---
+++ Data Structure and Algorithm/August 2023/August 03 2023/Calculate SubArrays divisible by n.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a[] = {1, 2, 3, 4, 5};
+    int n = sizeof(a) / sizeof(int);
+
+    int ans = 0;
+    for(int i = 0; i < n; i++){
+        for(int j = i; j < n; j++){
+            int sum = 0;
+            for(int k = i; k <= j; k++){
+                sum += a[k];
+            }
+
+            if(sum % n == 0){
+                ans++;
+            }
+        }
+    }
+    cout << ans;
+}
