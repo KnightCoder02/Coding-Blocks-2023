71be402d13b92512afbdc4cddf363384a599387b
Manav Aggarwal <53603898+Manav-Agg@users.noreply.github.com>
2023-08-06 16:14:06 +0530

    Add files via upload

--- Data Structure and Algorithm/LeetCode/4sum/4sum.cpp
+++ Data Structure and Algorithm/LeetCode/4sum/4sum.cpp
@@ -1,1 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a[] = {1, 0, -1, 0, -2, 2};
+    int n = sizeof(a) / sizeof(int);
+    int target = 0;
 
+    for(int i = 0; i < n - 3; i++){
+        for(int l = i + 1; l < n - 2; l++){
+            for(int j = l + 1; j < n - 1; j++){
+                for(int k = j + 1; k < n; k++){
+                    if(a[i] + a[j] + a[k] + a[l] == target){
+                        cout << a[i] << ", " << a[j] << ", " << a[k] << ", " << a[l] << endl;
+                    }
+                }
+            }
+        }
+    }
+}
