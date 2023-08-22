a9ef258ad707550d82595030ced6b49b69f84d2f
Manav Aggarwal <53603898+Manav-Agg@users.noreply.github.com>
2023-08-22 14:22:33 +0530

    Add files via upload

--- Data Structure and Algorithm/July 2023/July 24 Vectors/Merge two Arrays.cpp
+++ Data Structure and Algorithm/July 2023/July 24 Vectors/Merge two Arrays.cpp
@@ -4,6 +4,7 @@ void mergeSorted(int a[], int m, int b[], int n){
     int i = m - 1;
     int j = n - 1;
     int k = m + n - 1;
+
     while(i >= 0 && j >= 0){
         if(a[i] > b[j]){
             a[k] = a[i];
@@ -22,6 +23,12 @@ void mergeSorted(int a[], int m, int b[], int n){
         k--;
         j--;
     }
+
+    while(i >= 0){
+        a[k] = a[i];
+        k--;
+        i--;
+    }
 }
 
 void printArray(int a[], int n){
@@ -31,8 +38,8 @@ void printArray(int a[], int n){
 }
 
 int main(){
-    int a[7] = {3, 5, 8};
-    int m = 3;
+    int a[7] = {3, 5, 7, 8, 9};
+    int m = 5;
 
     int b[4] = {1, 2, 4, 6};
     int n = 4;
