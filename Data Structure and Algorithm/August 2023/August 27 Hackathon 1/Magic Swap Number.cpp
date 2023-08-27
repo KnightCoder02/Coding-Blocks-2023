f91154d705275ee441020d032162ad92a9eb7b66
Manav Aggarwal <53603898+Manav-Agg@users.noreply.github.com>
2023-08-27 21:45:08 +0530

    Add files via upload

--- Data Structure and Algorithm/August 2023/August 27 Hackathon 1/Magic Swap Number.cpp
+++ Data Structure and Algorithm/August 2023/August 27 Hackathon 1/Magic Swap Number.cpp
@@ -1,1 +1,23 @@
+#include<iostream>
+using namespace std;
 
+int largestNum(int num){
+	string str = to_string(num);
+	string temp = str;
+
+	for (int i = 0; i < str.size(); i++) {
+		for (int j = i + 1; j < str.size(); j++) {
+			swap(str[i], str[j]);
+			if (stoi(str) > stoi(temp))
+				temp = str;
+			swap(str[i], str[j]);
+		}
+	}
+	return stoi(temp);
+}
+
+int main() {
+	int num;
+    cin >> num;
+	cout << largestNum(num) << endl;
+}
