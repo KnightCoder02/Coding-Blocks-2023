d8aae6c8b9038ceea7887cbe7b252026883d56fc
Manav Aggarwal <53603898+Manav-Agg@users.noreply.github.com>
2023-08-29 20:20:12 +0530

    Add files via upload

--- Data Structure and Algorithm/August 2023/Assignment 3 Strings/String - Toggle Case.cpp
+++ Data Structure and Algorithm/August 2023/Assignment 3 Strings/String - Toggle Case.cpp
@@ -1,1 +1,19 @@
+#include<iostream>
+using namespace std;
+void toggle(char c[]){
+    for(int i=0; c[i]!='\0'; i++){
+        if(c[i] >= 'A' && c[i] <= 'Z'){
+            c[i] = tolower(c[i]);
+        }
+        else{
+            c[i] = toupper(c[i]);
+        }
+    }
+    cout << c;
+}
 
+int main(){
+    char c[1000];
+    cin >> c;
+    toggle(c);
+}
