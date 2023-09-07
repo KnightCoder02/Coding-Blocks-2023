64e4ad412b375442eb7f37e7f7e99616707822ab
Manav Aggarwal <53603898+Manav-Agg@users.noreply.github.com>
2023-09-07 23:27:43 +0530

    Update Check Prime.cpp

--- Data Structure and Algorithm/Assignments/1. Basics/Check Prime.cpp
+++ Data Structure and Algorithm/Assignments/1. Basics/Check Prime.cpp
@@ -1,21 +1,16 @@
 // Take as input a number N, print "Prime" if it is prime if not Print "Not Prime".
 #include<iostream>
 using namespace std;
-int main(){
-    int n, i = 2;
-    cin >> n;
+int main() {
+	int n, i = 2;
+	cin >> n;
 
-    if(n == 1){
-        cout << "Not allowed";
-        return 0;
-    }
-    
-    while(i <= n - 1){
-        if(n % i == 0){
-            cout << "Not Prime";
-            return 0;
-        }
-        i = i + 1;
-    }
-    cout << "Prime";
+	while(i <= n - 1){
+		if(n % i == 0){
+			cout << "Not Prime";
+			return 0;
+		}
+		i = i + 1;
+	}
+	cout << "Prime";
 }
