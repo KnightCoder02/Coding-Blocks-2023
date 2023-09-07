1dc19818c65549b8062eb7de13d5f6d1011b2535
Manav Aggarwal <53603898+Manav-Agg@users.noreply.github.com>
2023-09-07 23:14:29 +0530

    Update Hollow Diamond Pattern (Pattern 6).cpp

--- Data Structure and Algorithm/Assignments/1. Basics/Hollow Diamond Pattern (Pattern 6).cpp
+++ Data Structure and Algorithm/Assignments/1. Basics/Hollow Diamond Pattern (Pattern 6).cpp
@@ -14,8 +14,7 @@ int main(){
     for(int row = 1; row <= (n + 1) / 2; row++){
         if(row == 1){
             // Print n stars
-            for (int i = 0; i < n; i++)
-            {
+            for(int i = 0; i < n; i++){
                 cout << "*\t";
             }
         }
@@ -24,10 +23,12 @@ int main(){
             for(int i = 0; i < (n + 1) / 2 - row + 1; i++){
                 cout << "*\t";
             }
+
             // 2. Print 2*row-3 times spaces
             for(int i = 0; i < 2*row-3; i++){
                 cout << "\t";
             }
+
             // 3. Print (n + 1) / 2 - row + 1 times star
             for(int i = 0; i < (n + 1) / 2 - row + 1; i++){
                 cout << "*\t";
@@ -39,8 +40,7 @@ int main(){
     for(int row = (n + 1) / 2 - 1; row >= 1; row--){
         if(row == 1){
             // Print n stars
-            for (int i = 0; i < n; i++)
-            {
+            for(int i = 0; i < n; i++){
                 cout << "*\t";
             }
         }
@@ -49,10 +49,12 @@ int main(){
             for(int i = 0; i < (n + 1) / 2 - row + 1; i++){
                 cout << "*\t";
             }
+
             // 2. Print 2*row-3 times spaces
             for(int i = 0; i < 2*row-3; i++){
                 cout << "\t";
             }
+
             // 3. Print (n + 1) / 2 - row + 1 times star
             for(int i = 0; i < (n + 1) / 2 - row + 1; i++){
                 cout << "*\t";
