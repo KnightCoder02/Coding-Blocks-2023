c97b8631dd3fb2770ded5e344fd38dbef4bd4abd
Manav Aggarwal <53603898+Manav-Agg@users.noreply.github.com>
2023-09-07 22:52:59 +0530

    Update Find Largest of 3 numbers.cpp

--- Data Structure and Algorithm/Assignments/1. Basics/Find Largest of 3 numbers.cpp
+++ Data Structure and Algorithm/Assignments/1. Basics/Find Largest of 3 numbers.cpp
@@ -2,16 +2,14 @@
 #include<climits>
 using namespace std;
 int main(){
-    int lar = INT_MIN, i = 1;
-    while(i <= 3){
-        int no;
-        cin >> no;
-
-        if(no > lar){
-            lar = no;
-        }
-        i++;
-    }
-
-    cout << lar;
+	int lar = INT_MIN;
+	int no;
+	
+	for(int i = 0; i < 3; i++){
+		cin >> no;
+		if(no > lar){
+			lar = no;
+		}
+	}
+	cout << lar;
 }
