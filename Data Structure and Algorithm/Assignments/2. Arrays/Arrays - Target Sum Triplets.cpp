ac111232503a6a06e14931375fef1f116314aac0
Manav Aggarwal <53603898+Manav-Agg@users.noreply.github.com>
2023-09-08 01:16:40 +0530

    Update Arrays - Target Sum Triplets.cpp

--- Data Structure and Algorithm/Assignments/2. Arrays/Arrays - Target Sum Triplets.cpp
+++ Data Structure and Algorithm/Assignments/2. Arrays/Arrays - Target Sum Triplets.cpp
@@ -1,37 +1,40 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
+#include<iostream>
+#include<vector>
+#include<algorithm>
 using namespace std;
-
-void findTriplets(vector<int>& arr, int target) {
+void findTriplets(vector<int>& arr, int target){
     int n = arr.size();
 
     for(int i = 0; i < n - 2; ++i){
         // Skip duplicates
-        if(i > 0 && arr[i] == arr[i - 1])
+        if(i > 0 && arr[i] == arr[i - 1]){
             continue;
+        }
 
         int left = i + 1;
         int right = n - 1;
 
         while(left < right){
             int sum = arr[i] + arr[left] + arr[right];
-            if(sum == target) {
+            if(sum == target){
                 cout << arr[i] << ", " << arr[left] << " and " << arr[right] << endl;
 
                 // Skip duplicates
-                while(left < right && arr[left] == arr[left + 1])
+                while(left < right && arr[left] == arr[left + 1]){
                     ++left;
-                while (left < right && arr[right] == arr[right - 1])
+                }
+
+                while(left < right && arr[right] == arr[right - 1]){
                     --right;
+                }
 
                 ++left;
                 --right;
             }
-            else if (sum < target){
+            else if(sum < target){
                 ++left;
             }
-            else {
+            else{
                 --right;
             }
         }
