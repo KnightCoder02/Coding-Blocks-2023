b1656087719dd2d833f1397d4e8e8605f13ffaf7
Manav Aggarwal <53603898+Manav-Agg@users.noreply.github.com>
2023-09-07 23:32:07 +0530

    Update Print Series.cpp

--- Data Structure and Algorithm/Assignments/1. Basics/Print Series.cpp
+++ Data Structure and Algorithm/Assignments/1. Basics/Print Series.cpp
@@ -2,14 +2,13 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int i = 1, count = 0;
+    int n = 1, i = 1, count = 0;
     int num1, num2;
     cin >> num1 >> num2;
 
     while(true){
-        int x = 3 * i + 2;
-        if(x % num2 != 0){
-            cout << x << endl;
+        if((3 * i + 2) % num2 != 0){
+            cout << (3 * i + 2) << endl;
             count++;
         }
 
