fbdd491a91c08f110ff9b7f28add4b6d7dfc13ac
Manav Aggarwal <53603898+Manav-Agg@users.noreply.github.com>
2023-09-25 18:13:03 +0530

    Add files via upload

---
+++ Data Structure and Algorithm/September 2023/September 12 Linked List/Linked List using DMA (Insert At Front).cpp
@@ -0,0 +1,54 @@
+#include<iostream>
+using namespace std;
+class node{
+    public:
+    int data;
+    node* next;
+
+    node(int d){
+        data = d;
+        next = NULL;
+    }
+};
+
+// INSERTION IN LINKED LIST
+void insertAtFront(node* &head, node* &tail, int data){
+    if(head == NULL){ //Linked List is empty
+        node* n = new node(data);
+        head = tail = n;
+    }
+    else{
+        // 1. Create a node
+        node* n = new node(data);
+
+        // 2. Connect the node at correct place
+        n->next = head;
+
+        // 3. Update head
+        head = n;
+    }
+}
+
+void insertAtMiddle(){}
+
+void insertAtEnd(){}
+
+// PRINTING LINKED LIST
+void printLL(node* head){
+    while(head != NULL){
+        cout << head->data << " --> ";
+        head = head->next;
+    }
+    cout << "NULL\n";
+}
+
+int main(){
+    node* head, *tail;
+    head = tail = NULL;
+    
+    insertAtFront(head, tail, 1);
+    insertAtFront(head, tail, 2);
+    insertAtFront(head, tail, 3);
+    
+    printLL(head);
+}
--- Data Structure and Algorithm/September 2023/September 12 Linked List/Linked List.cpp
+++ Data Structure and Algorithm/September 2023/September 12 Linked List/Linked List.cpp
@@ -1,1 +1,23 @@
+#include<iostream>
+using namespace std;
+class node{
+    public:
+    int data;
+    node* next; //Self referential class 
+};
 
+int main(){
+    node a, b, c;
+
+    a.data = 1;
+    b.data = 2;
+    c.data = 3;
+
+    a.next = &b;
+    b.next = &c;
+    c.next = NULL;
+
+    // Printing linkedlist
+    // cout << a.data << " --> " << (*a.next).data << " --> NULL\n"; //Both are same
+    cout << a.data << " --> " << a.next->data << " --> " << a.next -> next -> data << " --> NULL";
+}
