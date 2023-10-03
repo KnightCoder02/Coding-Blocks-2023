322c88681a871138b16cfc5073d68b20855f68cd
Manav Aggarwal <53603898+Manav-Agg@users.noreply.github.com>
2023-10-03 14:17:36 +0530

    Update Subset_problem (Recursion).cpp

--- Data Structure and Algorithm/Assignments/5. Recursion/Subset_problem (Recursion).cpp
+++ Data Structure and Algorithm/Assignments/5. Recursion/Subset_problem (Recursion).cpp
@@ -1,69 +1,35 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
 using namespace std;
 
-// Function to print a vector
-void printVector(const vector<int>& subset){
-    for(int i = 0; i < subset.size(); i++){
-        cout << subset[i] << " ";
-    }
-    cout << endl;
-}
-
-// Part a: Recursive function to find subsets that sum to the target and print them
-void findSubsetsWithSum(vector<int>& arr, int index, int target, vector<int>& subset){
-    if(target == 0){
-        printVector(subset);
+int count = 0;
+void printSubsetSum(int *arr, int n, int i, int currentSum, int targetSum, string currentSubset) {
+    if(i == n){
+        if(currentSum == targetSum){
+            cout << currentSubset << " ";
+            count ++;
+        }
         return;
     }
 
-    if(index == arr.size() || target < 0){
-        return;
-    }
+    // Include the current element in the subset.
+    printSubsetSum(arr, n, i + 1, currentSum + arr[i], targetSum, currentSubset + ((currentSubset.size() > 0)? " " : "") + to_string(arr[i]));
 
-    // Include the current element in the subset
-    subset.push_back(arr[index]);
-    findSubsetsWithSum(arr, index + 1, target - arr[index], subset);
-
-    // Exclude the current element from the subset
-    subset.pop_back();
-    findSubsetsWithSum(arr, index + 1, target, subset);
+    // Exclude the current element from the subset.
+    printSubsetSum(arr, n, i + 1, currentSum, targetSum, currentSubset);
 }
 
-// Part b: Recursive function to count subsets that sum to the target
-int countSubsetsWithSum(vector<int>& arr, int index, int target){
-    if(target == 0){
-        return 1;
-    }
+int main() {
+    int n;
+    cin >> n;
 
-    if(index == arr.size() || target < 0){
-        return 0;
-    }
-
-    // Include the current element in the subset
-    int includeCount = countSubsetsWithSum(arr, index + 1, target - arr[index]);
-
-    // Exclude the current element from the subset
-    int excludeCount = countSubsetsWithSum(arr, index + 1, target);
-
-    return includeCount + excludeCount;
-}
-
-int main(){
-    int N;
-    cin >> N;
-    vector<int> arr(N);
-
-    for(int i = 0; i < N; i++){
+    int arr[n];
+    for (int i = 0; i < n; i++) {
         cin >> arr[i];
     }
 
-    int target;
-    cin >> target;
+    int targetSum;
+    cin >> targetSum;
 
-    vector<int> subset;
-    findSubsetsWithSum(arr, 0, target, subset);
-
-    int count = countSubsetsWithSum(arr, 0, target);
-    cout << count << endl;
+    printSubsetSum(arr, n, 0, 0, targetSum, "");
+    cout << endl << count;
 }
