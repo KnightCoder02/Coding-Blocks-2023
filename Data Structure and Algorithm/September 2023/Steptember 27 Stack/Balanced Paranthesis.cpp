e0f6330428dca66bd8dc33710445de60e5901d34
Manav Aggarwal <53603898+Manav-Agg@users.noreply.github.com>
2023-10-17 20:29:11 +0530

    Add files via upload

--- Data Structure and Algorithm/September 2023/Steptember 27 Stack/Balanced Paranthesis.cpp
+++ Data Structure and Algorithm/September 2023/Steptember 27 Stack/Balanced Paranthesis.cpp
@@ -1,1 +1,53 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+bool isBalanced(string x){
+    stack<char> s;
 
+    for(int i = 0; i < x.size(); i++){
+        char ch = x[i];
+        switch(ch){
+            case '(' : case '{' : case '[' : 
+                s.push(ch);             break;
+            case '}' :
+                if(!s.empty() && s.top() == '{'){
+                    s.pop();
+                }
+                else{
+                    return false;
+                }                       break;
+            case ')' :
+                if(!s.empty() && s.top() == '('){
+                    s.pop();
+                }
+                else{
+                    return false;
+                }                       break;
+            case ']' :
+                if(!s.empty() && s.top() == '['){
+                    s.pop();
+                }
+                else{
+                    return false;
+                }                       break;
+        }
+    }
+
+    if(s.empty()){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+int main(){
+    string s = "[a+b*{c+(d-e)*f})]";
+
+    if(isBalanced(s)){
+        cout << "Balanced";
+    }
+    else{
+        cout << "Not balanced";
+    }
+}
