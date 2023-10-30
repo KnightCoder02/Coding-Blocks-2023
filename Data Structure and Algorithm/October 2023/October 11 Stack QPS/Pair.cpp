43b2523a5d76ab6295aa6e83228580572401ecdb
Manav Aggarwal <53603898+Manav-Agg@users.noreply.github.com>
2023-10-30 18:48:45 +0530

    Add files via upload

--- Data Structure and Algorithm/October 2023/October 11 Stack QPS/Pair.cpp
+++ Data Structure and Algorithm/October 2023/October 11 Stack QPS/Pair.cpp
@@ -1,1 +1,25 @@
+#include<iostream>
+using namespace std;
+template<typename t1, typename t2>
+class Pair{
+    public:
+    t1 x;
+    t2 y;
+};
 
+int main(){
+    pair<int, int> p;   
+    p.first = 10;
+    p.second = 30;
+    cout << p.first << ", " << p.second << endl;
+
+    pair<int, string> p1;
+    p1.first = 100;
+    p1.second = "Hello";
+    cout << p1.first << ", " << p1.second << endl;
+
+    Pair<int, string> p2;
+    p2.x = 11;
+    p2.y = "World";
+    cout << p2.x << ", " << p2.y;
+}
