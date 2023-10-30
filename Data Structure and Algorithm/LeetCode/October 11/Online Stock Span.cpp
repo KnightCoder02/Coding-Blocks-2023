9db97234b8a00ffe8d3c83e89098dc1e02323fa3
Manav Aggarwal <53603898+Manav-Agg@users.noreply.github.com>
2023-10-30 18:45:14 +0530

    Add files via upload

--- Data Structure and Algorithm/LeetCode/October 11/Online Stock Span.cpp
+++ Data Structure and Algorithm/LeetCode/October 11/Online Stock Span.cpp
@@ -1,1 +1,29 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+class Pair{
+    public:
+    int price;
+    int span;
+};
 
+class StockSpanner{
+    public:
+    stack<Pair> s;
+    StockSpanner(){
+
+    }
+
+    int next(int price){
+        int span = 1;
+        while(!s.empty() && s.top().price <= price){
+            span += s.top().span;
+            s.pop();
+        }
+
+        s.push({price, span});
+        return span;
+    }
+};
+
+int main(){}
