9321206a558dd3a1030f5fce2844b769947b1ab8
Manav Aggarwal <53603898+Manav-Agg@users.noreply.github.com>
2023-11-13 17:03:12 +0530

    Update Merge Sorted Linked Lists.cpp

--- Data Structure and Algorithm/Assignments/6. Linked List/Merge Sorted Linked Lists.cpp
+++ Data Structure and Algorithm/Assignments/6. Linked List/Merge Sorted Linked Lists.cpp
@@ -1,93 +1,115 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-
-// Definition for a singly-linked list node.
-struct ListNode{
-    int val;
-    ListNode* next;
-    ListNode(int x) : val(x), next(nullptr) {}
+class node{
+    public:
+    int data;
+    node* next;
+    
+    node(int d){
+        data = d;
+        next = NULL;
+    }
 };
 
-// Function to merge two sorted linked lists
-ListNode* mergeSortedLists(ListNode* l1, ListNode* l2){
-    ListNode dummy(0);
-    ListNode* current = &dummy;
-
-    while(l1 && l2){
-        if(l1->val < l2->val){
-            current->next = l1;
-            l1 = l1->next;
-        }
-        else{
-            current->next = l2;
-            l2 = l2->next;
-        }
-
-        current = current->next;
-    }
-
-    if(l1){
-        current->next = l1;
+void insert(node* &head, node* &tail, int data){
+    if(head == NULL){
+        node* n = new node(data);
+        head = tail = n;
     }
     else{
-        current->next = l2;
+        node* n = new node(data);
+        tail->next = n;
+        tail = n;
     }
-
-    return dummy.next;
 }
 
-// Function to print the merged linked list
-void printMergedLinkedList(ListNode* head){
-    while(head){
-        cout << head->val << " ";
-        head = head->next;
+node* midLL(node* head){
+	if(head == NULL || head->next == NULL){
+		return head;
+	}
+
+	node* slow = head;
+	node* fast = head->next;
+
+	while(fast != NULL && fast->next != NULL){
+		fast = fast->next->next;
+		slow = slow->next;
+	}
+	return slow;
+}
+
+node* mergeLL(node* a, node* b){
+    if(!a){
+        return b;
     }
 
-    cout << endl;
+    if(!b){
+        return a;
+    }
+
+    node* nH;
+    if(a->data < b->data){
+        nH = a;
+        nH->next = mergeLL(a->next, b);
+    }
+    else{
+        nH = b;
+        nH->next = mergeLL(a, b->next);
+    }
+    return nH;
+}
+
+node* mergeSort(node* head){
+    if(!head || !head->next){
+        return head;
+    }
+
+    node *m = midLL(head);
+    node* a = head;
+    node* b = m->next;
+    m->next = NULL;
+    a = mergeSort(a);
+    b = mergeSort(b);
+    node* nH = mergeLL(a,b);
+    return nH;
+}
+
+void printLL(node* temp){
+	while(temp != NULL){
+		cout << temp->data << " ";
+		temp = temp->next;
+	}
 }
 
 int main(){
+    node *head,*tail;
+    node *head1,*tail1;
+    
     int t;
     cin >> t;
-
-    while(t--){
+    
+    for(int i = 0; i < t; i++){
+        head = tail = NULL;
+        head1 = tail1 = NULL;
+        
         int n1, n2;
         cin >> n1;
-        ListNode* l1 = nullptr;
-        ListNode* current = nullptr;
+        int data;
         
         for(int i = 0; i < n1; i++){
-            int val;
-            cin >> val;
-            
-            if(l1 == nullptr){
-                l1 = new ListNode(val);
-                current = l1;
-            }
-            else{
-                current->next = new ListNode(val);
-                current = current->next;
-            }
+            cin >> data;
+            insert(head, tail, data);
         }
-
-        cin >> n2;
-        ListNode* l2 = nullptr;
-        current = nullptr;
         
+        cin >> n2;
         for(int i = 0; i < n2; i++){
-            int val;
-            cin >> val;
-            if(l2 == nullptr){
-                l2 = new ListNode(val);
-                current = l2;
-            }
-            else{
-                current->next = new ListNode(val);
-                current = current->next;
-            }
+            cin >> data;
+            insert(head1, tail1, data);
         }
 
-        ListNode* mergedList = mergeSortedLists(l1, l2);
-        printMergedLinkedList(mergedList);
+        head = mergeLL(head, head1);
+        node* nH = mergeSort(head);
+        printLL(nH);
+        cout << endl;
     }
 }
