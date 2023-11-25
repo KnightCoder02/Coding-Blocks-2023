f0e97c7b5fce5c9c0b4bbbe6a33b7258f92a7bf9
Manav Aggarwal <53603898+Manav-Agg@users.noreply.github.com>
2023-11-25 14:07:34 +0530

    Add files via upload

--- Data Structure and Algorithm/LeetCode/November/November 16/Valid Anagram.cpp
+++ Data Structure and Algorithm/LeetCode/November/November 16/Valid Anagram.cpp
@@ -1,1 +1,28 @@
+#include<iostream>
+#include<unordered_map>
+using namespace std;
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.length() != t.length()){
+            return false;
+        }
 
+        unordered_map<char, int> mp;
+        for(int i = 0; i < s.size(); i++){
+            mp[s[i]]++;
+            mp[t[i]]--;
+        }
+
+        for(auto &x : mp){
+            if(x.second != 0){
+                return false;
+            }
+        }
+        return true;
+    }
+};
+
+int main(){
+    Solution sol;
+}
