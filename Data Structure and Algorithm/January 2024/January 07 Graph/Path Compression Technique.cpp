b03b96310f7cd6c7a9babd899211d7f3a738be32
Manav Aggarwal <53603898+Manav-Agg@users.noreply.github.com>
2024-03-10 13:02:59 +0530

    Add files via upload

---
+++ Data Structure and Algorithm/January 2024/January 07 Graph/Path Compression Technique.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class DSU{
+    vector<int> parent;
+
+    public:
+    DSU(int v){
+        parent.resize(v + 1);
+
+        // Initially all are superParent / Leader / representative themselves.
+        for(int i = 1; i <= v; i++){
+            make_set(i);
+        }
+    }
+
+    void make_set(int u){
+        parent[u] = u;
+    }
+
+    // To return the leader of the u
+    int find_set(int u){
+        // base case
+        if(u == parent[u]){
+            return u;
+        }
+
+        // recursive case
+        return parent[u] = find_set(parent[u]); // Path Compression Technique
+    }
+
+    void union_set(int u, int v){
+        int leader_u = find_set(u);
+        int leader_v = find_set(v);
+
+        parent[leader_v] = leader_u;
+    }
+};
+
+int main(){
+    DSU d(5);
+    d.union_set(1, 2);
+    d.union_set(3, 4);
+    d.union_set(1, 3);
+
+    cout << d.find_set(5);
+}
