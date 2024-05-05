2e6c548ecc03e331875bde107a9b4b25eb2e2309
Manav Aggarwal <53603898+Manav-Agg@users.noreply.github.com>
2024-05-05 17:24:20 +0530

    Add files via upload

--- Web Development/February 2024/February 12 JavaScript/Arrays/script.js
+++ Web Development/February 2024/February 12 JavaScript/Arrays/script.js
@@ -1,1 +1,22 @@
+let arr = [1,2,3,4,5];
+console.log(arr);
 
+console.log("Length: ", arr.length);
+
+for(let i = 0; i < arr.length; i++){
+    console.log(arr[i]);
+}
+
+let str = '';
+for(let i = 0; i < arr.length; i++){
+    str += arr[i] + ' ';
+}
+console.log(str);
+
+// We can take input from cmd line in Js only
+console.log(process.argv);
+
+let inp1 = process.argv[2];
+let inp2 = process.argv[3];
+let inp3 = process.argv[4];
+console.log(inp1, inp2, inp3);
