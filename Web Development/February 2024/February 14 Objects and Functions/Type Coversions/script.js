a578e67c9fa87d2b991d588a1379a702ef417bab
Manav Aggarwal <53603898+Manav-Agg@users.noreply.github.com>
2024-05-13 09:58:51 +0530

    Add files via upload

--- Web Development/February 2024/February 14 Objects and Functions/Type Coversions/script.js
+++ Web Development/February 2024/February 14 Objects and Functions/Type Coversions/script.js
@@ -1,1 +1,12 @@
+console.log("0 == \\t :", 0 == '\t');   //true, because of typeconversion
+console.log("0 == \\n :", 0 == '\n');   //true, because of typeconversion
+console.log("\\t == \\n :", '\t' == '\n');   //false, because of no typeconversion
 
+// To convert a value to a number we can use + sign before it
+console.log("+\\t :", +'\t'); 
+console.log("+\\n :", +'\n');
+
+console.log("[] == 0 :", [] == 0);  //true
+console.log("\t == 0 :", '\t' == 0);  //true
+console.log("[] == \\t :", [] == '\t');  //false
+console.log("+[] == \\t :", +[] == '\t');  //true
