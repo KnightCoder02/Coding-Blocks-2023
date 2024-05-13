d638c089cd1e1395a477a2a1237910950bc7c790
Manav Aggarwal <53603898+Manav-Agg@users.noreply.github.com>
2024-05-13 09:56:23 +0530

    Add files via upload

--- Web Development/February 2024/February 14 Objects and Functions/Arrays/arrays.js
+++ Web Development/February 2024/February 14 Objects and Functions/Arrays/arrays.js
@@ -1,1 +1,30 @@
+let arr = [1, 2, 3, 4, "Hello", true, 1.11];
+console.log(arr);
 
+arr.unshift("Happy");
+console.log(arr);
+
+arr.shift("Happy");
+console.log(arr);
+
+arr.push("Valentine");
+console.log(arr);
+
+arr.pop();
+console.log(arr);
+
+// for of loop
+for(let el of arr){
+    console.log(el);
+}
+
+for(let inx in arr){
+    console.log(inx);
+}
+
+// Searching Key
+let indx = arr.indexOf("Hello");
+console.log("Index: ", indx);
+
+let inx = arr.indexOf("Hell"); // value does not exist then it prints -1
+console.log("Index: ", inx);
