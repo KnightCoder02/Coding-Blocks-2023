a98e704b33367e87027712fb2cf924feaff225da
Manav Aggarwal <53603898+Manav-Agg@users.noreply.github.com>
2024-05-13 09:58:11 +0530

    Add files via upload

---
+++ Web Development/February 2024/February 14 Objects and Functions/Strings/InbuiltFunctions.js
@@ -0,0 +1,11 @@
+let str = "Hello World Learning Strings";
+let arr = str.split(" ");
+console.log(arr);
+
+// To join array into strings
+let newStr = arr.join("_");
+console.log(newStr);
+
+// To find index of a word
+let inx = str.indexOf("World");
+console.log(inx);
--- Web Development/February 2024/February 14 Objects and Functions/Strings/script.js
+++ Web Development/February 2024/February 14 Objects and Functions/Strings/script.js
@@ -1,1 +1,14 @@
+let str = "Hello World Learning Strings";
+console.log(str);
 
+for(let i = 0; i < str.length; i++){
+    console.log(str[i]);
+}
+
+for(let inx in str){
+    console.log(str[inx]);
+}
+
+for(let el of str){
+    console.log(el);
+}
