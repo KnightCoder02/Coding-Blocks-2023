164bf34e25cf20fca97f898b7081a840fc7a0cf5
Manav Aggarwal <53603898+Manav-Agg@users.noreply.github.com>
2024-05-13 09:57:37 +0530

    Add files via upload

--- Web Development/February 2024/February 14 Objects and Functions/Objects/script.js
+++ Web Development/February 2024/February 14 Objects and Functions/Objects/script.js
@@ -1,1 +1,19 @@
+let Obj = {
+    a: 1,
+    "Hello World": "Earth",
+    "": "Empty String",
+    " ": "Space",
+    "Hello":"World"
+};
+
+console.log(Obj.a);
+console.log(Obj.Hello);
+// console.log(Obj."Hello World"); //This don't work
+console.log(Obj["Hello World"]);
+
+
+// for in loop
+for(let k in Obj){
+    console.log(Obj[k]);
+}
 
