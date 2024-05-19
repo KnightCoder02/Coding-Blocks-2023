a747727d7db3cc4e07976da5d75611f6adad95e4
Manav Aggarwal <53603898+Manav-Agg@users.noreply.github.com>
2024-05-19 15:21:07 +0530

    Add files via upload

--- Web Development/February 2024/February 15 Functions/LetVsVar/script.js
+++ Web Development/February 2024/February 15 Functions/LetVsVar/script.js
@@ -1,1 +1,26 @@
+// let is block scope, can;t be hoisting
+// Var is functional scope, can be hoisting
 
+for(let i = 0; i < 5; i++){
+    console.log(i);
+}
+
+console.log(i); //Error
+
+for(var j = 0; j < 5; j++){
+    console.log(j);
+}
+
+console.log(j);
+
+let x = 1;
+function fun(){
+    console.log(x);
+
+    let x = 1;
+    console.log(x);
+    x++;
+}
+
+fun();
+console.log(x);
