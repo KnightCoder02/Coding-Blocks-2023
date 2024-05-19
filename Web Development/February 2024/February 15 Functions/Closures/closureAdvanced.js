0635e1ef41ee20d10b5cc5b4df18734927aaa5ca
Manav Aggarwal <53603898+Manav-Agg@users.noreply.github.com>
2024-05-19 15:20:30 +0530

    Add files via upload

---
+++ Web Development/February 2024/February 15 Functions/Closures/closureAdvanced.js
@@ -0,0 +1,23 @@
+function grandFather(){
+    var x = 1;
+
+    function parent(){
+        x++;
+
+        function child(){
+            x++;
+            console.log(x);
+        }
+        return child;
+    }
+    return parent;
+}
+
+let fun = grandFather();
+let f1 = fun();
+let f2 = fun();
+
+f1();
+f1();
+f2();
+f2();
