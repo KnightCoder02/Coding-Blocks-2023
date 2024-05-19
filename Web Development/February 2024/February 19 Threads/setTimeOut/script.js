3f40bf294b7a91b4514dadbfc00038a2e17571e8
Manav Aggarwal <53603898+Manav-Agg@users.noreply.github.com>
2024-05-19 15:29:24 +0530

    Add files via upload

---
+++ Web Development/February 2024/February 19 Threads/setTimeOut/script.js
@@ -0,0 +1,11 @@
+// setTimeOut(function(){
+// }, millisecond)
+
+setTimeOut(function(){
+    console.log("Hello World")
+}, 5000);
+
+console.log("Hiii")
+console.log("How r u?")
+console.log("what are you doing?")
+console.log("where are you going?")
