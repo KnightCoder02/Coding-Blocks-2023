da890f234ac27bcf2b7ab3f85e676124bb8e61f0
Manav Aggarwal <53603898+Manav-Agg@users.noreply.github.com>
2024-05-26 19:09:49 +0530

    Add files via upload

--- Web Development/February 2024/February 26 Arrow Function and Binding/Arrow Function/arrow.js
+++ Web Development/February 2024/February 26 Arrow Function and Binding/Arrow Function/arrow.js
@@ -1,1 +1,14 @@
+let fun = (name, age)=>{
+    console.log(name, age);
+}
 
+fun("Manav", 24);
+
+let sum = (a, b)=>{ //User returns a value
+    return a + b;
+}
+
+console.log(sum(10, 20));
+
+let sub = (a, b)=>a-b; //Default return
+console.log(sub(20, 10));
