86b4769da8304f39f2c37c28c84b8e6897c6607e
Manav Aggarwal <53603898+Manav-Agg@users.noreply.github.com>
2024-05-26 18:36:33 +0530

    Add files via upload

---
+++ Web Development/February 2024/February 22 Callback and Promises/AsyncCodeIssue/callBackHell.js
@@ -0,0 +1,14 @@
+console.log("Maggi lene gya");
+setTimeout(function(){
+    console.log("Maggi lekar aa gya");
+    console.log("Maggi bnani start");
+
+    setTimeout(function(){
+        console.log("Maggi Ban gyi");
+        console.log("Maggi Khana start");
+
+        setTimeout(function(){
+            console.log("Maggi khana samapt");
+        }, 2000);
+    }, 2000);
+}, 2000);
--- Web Development/February 2024/February 22 Callback and Promises/AsyncCodeIssue/script.js
+++ Web Development/February 2024/February 22 Callback and Promises/AsyncCodeIssue/script.js
@@ -1,1 +1,21 @@
+function mummyMaggiBanado(cb){
+    setTimeout(function(){
+        console.log("Mummy Maggi Bnado");
+        cb(maggiBanaStart); //This is a callback function of mummyMaggiBanado
+    }, 2000);
+}
 
+function jaaMarketSeMaggiLaa(cb){
+    setTimeout(function(){
+        console.log("Jakar Market se maggi lekar aa");
+        cb();
+    }, 2000);
+}
+
+function maggiBanaStart(){
+    setTimeout(function(){
+        console.log("Maggi Bnana started");
+    }, 2000);
+}
+
+mummyMaggiBanado(jaaMarketSeMaggiLaa);
