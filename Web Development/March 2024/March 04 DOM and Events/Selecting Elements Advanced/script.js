5b63048b4e2c0f78aaeee3c746f26bb9590b84cf
Manav Aggarwal <53603898+Manav-Agg@users.noreply.github.com>
2024-06-05 15:58:06 +0530

    Add files via upload

--- Web Development/March 2024/March 04 DOM and Events/Selecting Elements Advanced/script.js
+++ Web Development/March 2024/March 04 DOM and Events/Selecting Elements Advanced/script.js
@@ -1,16 +1,16 @@
-// window.onload = () => {
-    // 1. Selecting Elements by TagName
-    let divs = document.getElementsByTagName('div');
-    console.log(divs);
+// 1. Using query Selector: Will only give one element
+let movie = document.querySelector('.movie');
+let movies = document.querySelectorAll('.movie');
+console.log(movies);
 
-    // 2. Selecting Elements by Class
-    let movies = document.getElementsByClassName("movie");
-    console.log(movies);
+// Selecting id using query Selector
+let gadar = document.querySelector('#gadar');
+console.log(gadar);
 
-    // 3. Selecting Elements by ID
-    const avengers = document.getElementById("avengers");
-    console.log(avengers);
+// Selecting using TagName
+let div = document.querySelector('div');
+console.log(div);
 
-    let movieList = document.getElementById('movieList');
-    console.log(movieList);
-// }
+// Pick the li item that has the class ironman
+let ironman = document.querySelector('li.ironman');
+console.log(ironman);
