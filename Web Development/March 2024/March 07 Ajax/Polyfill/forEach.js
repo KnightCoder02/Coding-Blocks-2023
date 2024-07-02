a494f673e4b179f8c733791bde9ddcbdaa5825e9
Manav Aggarwal <53603898+Manav-Agg@users.noreply.github.com>
2024-07-02 18:12:09 +0530

    Add files via upload

--- Web Development/March 2024/March 07 Ajax/Polyfill/forEach.js
+++ Web Development/March 2024/March 07 Ajax/Polyfill/forEach.js
@@ -1,1 +1,22 @@
+let arr = [1, 2, 3, 4, 5, 6];
+let arr1 = [1, 2, 3, 4, "Hello", 6];
 
+Array.prototype.myforEach = function(fun){
+    let arr = this;
+    for(let i = 0; i < arr.length; i++){
+        fun(arr[i], i, arr);
+    }
+}
+
+arr.myforEach((data, indx, arr) => {
+    console.log("Index: ", indx, "Data: ", data, "Arr: ", arr);
+})
+
+arr1.myforEach((data, indx, arr) => {
+    console.log("Index: ", indx, "Data: ", data, "Arr: ", arr);
+})
+
+// Inbuilt forEach
+arr.forEach((data, indx, arr) => {
+    console.log("Index: ", indx, "Data: ", data, "Arr: ", arr);
+})
