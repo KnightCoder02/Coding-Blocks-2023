c9009800938ef44c23ce499efb9d875b311033f5
Manav Aggarwal <53603898+Manav-Agg@users.noreply.github.com>
2025-01-12 15:07:51 +0530

    Update script.js

--- Web Development/March 2024/March 14 Polyfills and JQuery/JQuery/script.js
+++ Web Development/March 2024/March 14 Polyfills and JQuery/JQuery/script.js
@@ -6,24 +6,25 @@ let b1 = $(b);
 console.log(b1);
 
 let h1 = $('<h1>');
-body.append(
-    h1.text('Hello World').css('font-size','54px')
-)
-.append(
-    $('<button>')
-        .text('Click Me')
-        .addClass('button')
-        .click(ev => {
-            $(ev.target).css('background-color', 'magenta');
-        })
-        .dblclick(ev => {
-            console.log("Why press double");
-        })
-)
-.append(
-    $('<ul>')
-        .html(
-            '<li>Avengers</li> <li>Thor</li> <li>Ironman</li>'
-        )
-        .addClass('movies')
-)
+body
+    .append(
+        h1.text('Hello World').css('font-size','54px')
+    )
+    .append(
+        $('<button>')
+            .text('Click Me')
+            .addClass('button')
+            .click(ev => {
+                $(ev.target).css('background-color', 'magenta');
+            })
+            .dblclick(ev => {
+                console.log("Why press double");
+            })
+    )
+    .append(
+        $('<ul>')
+            .html(
+                '<li>Avengers</li> <li>Thor</li> <li>Ironman</li>'
+            )
+            .addClass('movies')
+    )
