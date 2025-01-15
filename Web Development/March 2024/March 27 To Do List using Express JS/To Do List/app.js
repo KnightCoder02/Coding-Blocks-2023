453ed482e75db9908d9b0e110b2084ed1f7a795c
Manav Aggarwal <53603898+Manav-Agg@users.noreply.github.com>
2025-01-15 19:26:11 +0530

    Add files via upload

--- Web Development/March 2024/March 27 To Do List using Express JS/To Do List/app.js
+++ Web Development/March 2024/March 27 To Do List using Express JS/To Do List/app.js
@@ -1,1 +1,30 @@
+const express = require('express')
+const path = require('path');
+const app = express()
+const port = 3000;
 
+let publicPath = path.join(__dirname, "public");
+app.use(express.static(publicPath));
+app.use(express.json());
+
+let todos = ["Dancing", "Singing", "Coding", "Reading"];
+app.get('/todos', (req, res) => {
+    res.status(200).send(todos);
+})
+
+app.get('/addtask', (req, res) => {
+    let {task} = req.query;
+    // If task is already in list then don't add it to the array
+    if(!todos.includes(task)){
+        todos.push(task);
+    }
+    res.redirect('/todos');
+})
+
+app.use((req, res, next) => {
+    res.status(404).json("Error 404: Page not Found");
+})
+
+app.listen(port, () => {
+    console.log(`http://localhost:${port}`);
+});    //This will start the server
