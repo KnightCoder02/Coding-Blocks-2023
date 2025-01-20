833e0912020b12e17ef4de35cb0a440fcc4b793c
Manav Aggarwal <53603898+Manav-Agg@users.noreply.github.com>
2025-01-20 20:07:19 +0530

    Add files via upload

--- Web Development/April 2024/April 10 Multithreading/Multithreading/server.js
+++ Web Development/April 2024/April 10 Multithreading/Multithreading/server.js
@@ -1,1 +1,33 @@
+const express = require('express');
+const cluster = require('cluster');
+// Cluster module helps us to create multiple instance of node
+const os = require('os');
+const app = express();
 
+// No. of cores =>
+let totalCpus = os.cpus().length;
+let platform = os.platform();
+
+console.log(process.pid);
+console.log(cluster.isPrimary); //This will tell me if I am on main thread
+if(cluster.isPrimary){
+    // total cpus = no. of cores = 12
+    console.log('This is my main thread'); //This will tell me if I am on main thread
+    for(i = 0; i < totalCpus; i++){
+        cluster.fork();
+    }
+}
+else{
+    console.log(totalCpus, platform);
+
+    app.get('/', (req, res) => {
+        res.json({hi: ' response from node instance ${process.pid}'})
+    })
+
+    app.get('/slowApi', (req, res) => {
+        for(let i = 0; i < 10_000_000_000; i++){}
+        res.json({blocking: ' response from node instance ${process.pid}'});
+    })
+
+    app.listen(4000);
+}
