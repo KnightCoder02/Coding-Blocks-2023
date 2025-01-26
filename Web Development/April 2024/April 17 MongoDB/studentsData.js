f0edc86e04eb8e27b5b03c61a76bc7888d361008
Manav Aggarwal <53603898+Manav-Agg@users.noreply.github.com>
2025-01-26 14:31:22 +0530

    Add files via upload

---
+++ Web Development/April 2024/April 17 MongoDB/studentsData.js
@@ -0,0 +1,20 @@
+let students = [
+    {
+        name: 'Manav',
+        age: 25,
+        subject: 'Web Development',
+        marks: 80
+    },
+    {
+        name: 'Vashu',
+        age: 23,
+        subject: 'SSC',
+        marks: 70
+    },
+    {
+        name: 'Yogi',
+        age: 24,
+        subject: 'Navi',
+        marks: 90
+    }
+]
