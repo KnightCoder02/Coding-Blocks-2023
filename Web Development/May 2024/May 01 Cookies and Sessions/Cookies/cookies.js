8abf568dc9c00078a9ca265e8c47497646cc4edd
Manav Aggarwal <53603898+Manav-Agg@users.noreply.github.com>
2025-01-28 23:15:48 +0530

    Add files via upload

--- Web Development/May 2024/May 01 Cookies and Sessions/Cookies/cookies.js
+++ Web Development/May 2024/May 01 Cookies and Sessions/Cookies/cookies.js
@@ -1,1 +1,67 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+const port = 3000;
 
+app.set('view engine', 'hbs');
+app.use(express.urlencoded({extended: true}));
+
+const Cookies = require('cookies');
+const keys = ['Keyboard cat'];
+
+app.post('/login', (req, res) => {
+    // This will create a cookies
+    const {username} = req.body;
+    var cookies = new Cookies(req, res, {keys : keys});
+
+    cookies.set('user', JSON.stringify({
+        isAdmin: false,
+        username
+    }));
+    res.redirect('/profile');
+});
+
+// If we have logged in before then we cannot go to /login again
+app.get('/login', (req, res, next) => {
+    var cookies = new Cookies(req, res, {keys : keys});
+    let data = cookies.get('user');
+
+    if(!data){
+        return res.render('login');
+    }
+    res.redirect('/profile');
+});
+
+// We cannot access profile before login
+app.get('/profile', (req, res, next) => {
+    var cookies = new Cookies(req, res, {keys : keys});
+    let data = cookies.get('user');
+
+    if(!data){
+        return res.redirect('/login');
+    }
+    data = JSON.parse(data);
+    res.render('profile', {
+        username: data.username
+    });
+});
+
+app.get('/admin', (req, res) => {
+    var cookies = new Cookies(req, res, {keys : keys});
+    let data = cookies.get('user');
+
+    if(!data){
+        return res.redirect('/login');
+    }
+
+    data = JSON.parse(data);
+    if(data.isAdmin){
+        return res.render('admin');
+    }
+
+    res.redirect('/profile');
+})
+
+app.listen(port, () => {
+    console.log(`http://localhost:` + port);
+});
